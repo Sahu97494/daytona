e2fa314413bb5b07d3e1b73d7749bb1d10f84469
Toma Puljak <toma.puljak@hotmail.com>
2025-06-26 13:15:36 +0000

    wip: polyfill hell
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- libs/sdk-typescript/src/index.ts
+++ libs/sdk-typescript/src/index.ts
@@ -22,7 +22,7 @@ export { DaytonaError } from './errors/DaytonaError'
 export { Image } from './Image'
 export { Sandbox } from './Sandbox'
 export type { SandboxCodeToolbox } from './Sandbox'
-export { CreateSnapshotParams } from './Snapshot'
+export type { CreateSnapshotParams } from './Snapshot'
 
 // Chart and artifact types
 export { ChartType } from './types/Charts'
